bc203d838afa487773880506b33074e4cf517d19
Dev Bev <beverly.jaimespuente@gmail.com>
2020-01-23 22:12:30 -0600

    commented out old code to refactor

--- jQuery/jQuery_animation.js
+++ jQuery/jQuery_animation.js
@@ -1,5 +1,7 @@
-$('#flashMessage').hide();
-$('#flashMessage').fadeIn(1000);
-//give user time to read message
-$('#flashMessage').delay(3000);
-$('#flashMessage').slideUp();
+// $('#flashMessage').hide();
+// $('#flashMessage').slideDown(1000);
+// //give user time to read message
+// $('#flashMessage').delay(3000);
+// $('#flashMessage').slideUp();
+
+//the same can be done with less code by stringing
