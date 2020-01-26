f9440f9da6638ec40412eeb84a9a0eb3c7fa240d
Dev Bev <beverly.jaimespuente@gmail.com>
2020-01-25 18:15:46 -0600

    taking value from title user input

--- jQuery/jQuery_animation.js
+++ jQuery/jQuery_animation.js
@@ -6,19 +6,24 @@
 
 //the same can be done with less code by stringing
 
- $('#flashMessage')
-     .hide()
-     .slideDown(1000)
-     .delay(3000)
-     .slideUp();
+//  $('#flashMessage')
+//      .hide()
+//      .slideDown(1000)
+//      .delay(3000)
+//      .slideUp();
+//
+//   const title = "My First Blog Post";
+//   const content = "<strong>This is my first post!</strong>";
+//
+//   $('#blogTitlePreview').text(title);
+//   // $('#blogContentPreview').text(content);
+//
+//   //you can also save text but variables are cleaner
+// $('#blogContentPreview').html(content);
 
-  const title = "My First Blog Post";
-  const content = "<strong>This is my first post!</strong>";
-
-  $('#blogTitlePreview').text(title);
-  // $('#blogContentPreview').text(content);
-
-  //you can also save text but variables are cleaner
-$('#blogContentPreview').html(content);
+$('#previewButton').click(function () {
+const title = $('#blogTitleInput').val();
+ console.log(title);
+});
 
 
