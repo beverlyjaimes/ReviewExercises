9e00751c4705699dc16e829537d9c76b0943404f
Dev Bev <beverly.jaimespuente@gmail.com>
2020-01-31 19:19:51 -0600

    added hide to button inside click event function

--- jQuery/StarWarsSpoilers/js/sw.js
+++ jQuery/StarWarsSpoilers/js/sw.js
@@ -1,6 +1,8 @@
 //Hide spoiler on page load
 $('.spoiler span').hide();
-//When the button is pressed show spoiler and hide the button
+//When the button is pressed show spoiler
 $('.spoiler button').click(function () {
     $('.spoiler span').show();
+    // and hide the button
+    $('.spoiler button').hide();
 });
