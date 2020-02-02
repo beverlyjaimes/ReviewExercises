20f2585eddeccf344edb5bb4219fade46f6699c1
Dev Bev <beverly.jaimespuente@gmail.com>
2020-02-02 17:34:20 -0600

    added todo notes

--- jQuery/StarWarsSpoilers/js/sw.js
+++ jQuery/StarWarsSpoilers/js/sw.js
@@ -1,3 +1,7 @@
+//create reveal spoiler button
+//append to web page
+
+
 //Hide spoiler on page load
 $('.spoiler span').hide();
 //When the button is pressed show spoiler
@@ -5,4 +9,7 @@ $('.spoiler button').click(function () {
     $('.spoiler span').show();
     // and hide the button
     $('.spoiler button').hide();
+
 });
+// Unobtrusive JS
+//make sure website remains functional without js
