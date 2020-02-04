363e445d5a752c02e18c2455174758bd682b8409
Dev Bev <beverly.jaimespuente@gmail.com>
2020-02-03 23:31:50 -0600

    refactored using on method

--- jQuery/StarWarsSpoilers/js/sw.js
+++ jQuery/StarWarsSpoilers/js/sw.js
@@ -2,15 +2,15 @@
 const $button = $('<button>Reveal Spoiler</button>');
 //append to web page
 $('.spoiler').append($button);
+// Unobtrusive JS
+//make sure website remains functional without js
 
 //Hide spoiler on page load
 $('.spoiler span').hide();
 //When the button is pressed show spoiler
-$('.spoiler button').click(function () {
+$('.spoiler button').on('click', function () {
     $('.spoiler span').show();
     // and hide the button
     $('.spoiler button').hide();
 
 });
-// Unobtrusive JS
-//make sure website remains functional without js
