6348b5a6df977a9e9a1bca8f383922be2a325370
Dev Bev <beverly.jaimespuente@gmail.com>
2020-02-03 23:38:19 -0600

    refactoring

--- jQuery/StarWarsSpoilers/js/sw.js
+++ jQuery/StarWarsSpoilers/js/sw.js
@@ -1,3 +1,17 @@
+// //When the button is pressed show spoiler
+// $('.spoiler button').click( function () {
+//     //you can also add other methods ex: mouseleave
+//     $('.spoiler span').show();
+//     // and hide the button
+//     $('.spoiler button').hide();
+// //    add event propagation
+//
+// });
+
+$('.spoiler').on('click' , 'button', function () {
+//if user clicks on this button fire the function
+});
+
 //create reveal spoiler button
 const $button = $('<button>Reveal Spoiler</button>');
 //append to web page
@@ -7,10 +21,4 @@ $('.spoiler').append($button);
 
 //Hide spoiler on page load
 $('.spoiler span').hide();
-//When the button is pressed show spoiler
-$('.spoiler button').on('click', function () {
-    $('.spoiler span').show();
-    // and hide the button
-    $('.spoiler button').hide();
 
-});
