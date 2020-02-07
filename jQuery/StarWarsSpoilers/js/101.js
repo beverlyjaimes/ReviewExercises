a3afa7bc62c622a5bdbcc12350db88a5a205f156
Dev Bev <beverly.jaimespuente@gmail.com>
2020-02-06 23:34:55 -0600

    linked html and added shortest string

--- jQuery/StarWarsSpoilers/js/101.js
+++ jQuery/StarWarsSpoilers/js/101.js
@@ -17,3 +17,13 @@ const isOdd = function(num) {
     }
 };
 console.log(isOdd(5));
+
+// Write a function definition named shortestString that takes in an array of strings and returns the shortest string in the array.
+let arr = ["kiwi", "mango", "strawberry"];
+const shortestString = (arr) => {
+    var shortest = arr.reduce((shortestWord, currentWord) => {
+        return currentWord.length < shortestWord.length ? currentWord : shortestWord;
+    }, arr[0]);
+    return shortest;
+}
+console.log(shortestString(arr));
