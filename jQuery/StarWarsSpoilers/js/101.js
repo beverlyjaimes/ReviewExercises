681212310c1d087fef9d3e129abda6b5e0468c12
Dev Bev <beverly.jaimespuente@gmail.com>
2020-02-06 23:32:59 -0600

    fruits with js methods

---
+++ jQuery/StarWarsSpoilers/js/101.js
@@ -0,0 +1,10 @@
+// Exercise 5
+// Add the string "tomato" to the end of the fruits array.
+// *Hint* Recommend finding and using a built-in JS operation to add to an array rather than recreating the array.
+let fruits = ['mango', 'banana', 'guava', 'kiwi', 'strawberry'];
+fruits.push('tomato');
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+fruits.sort().reverse();
+console.log(fruits);
