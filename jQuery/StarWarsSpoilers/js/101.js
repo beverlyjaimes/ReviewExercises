3da4126363ed0f055c56104d8f911090265c030d
Dev Bev <beverly.jaimespuente@gmail.com>
2020-02-06 23:40:30 -0600

    evens using filter

--- jQuery/StarWarsSpoilers/js/101.js
+++ jQuery/StarWarsSpoilers/js/101.js
@@ -28,11 +28,18 @@ const shortestString = (arr) => {
 };
 console.log(shortestString(arr));
 
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var evens = [];
+// for (var i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 === 0) {
+//         evens.push(numbers[i]);
+//     }
+// }
+// console.log(evens);
+
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var evens = [];
-for (var i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] % 2 === 0) {
-        evens.push(numbers[i]);
-    }
-}
-console.log(evens)
+
+var evens = numbers.filter(function(n) {
+    return n % 2 === 0;
+});
+console.log(evens);
